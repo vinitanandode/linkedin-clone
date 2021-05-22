99eb05f0146771ceae4b67bfe5665bc194c47fa2
Vinita Nandode <vinita.nandode@gmail.com>
2021-05-23 01:03:01 +0530

    implemented sections

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,55 +1,82 @@
-import styled from 'styled-components';
-import style from 'styled-components';
+import styled from "styled-components";
+import style from "styled-components";
+import LeftSide from "./LeftSide";
+import Main from "./Main";
+import RightSide from "./RightSide";
 
 const Home = (props) => {
-    return(
-        <Container>
-            <Section>
-                <h5><a>Hiring in a hurry? - </a></h5>
-                <p>Find talented pros in records time with Upwork and keep business moving</p>
-            </Section>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Section>
+        <h5>
+          <a>Hiring in a hurry? - </a>
+        </h5>
+        <p>
+          Find talented pros in records time with Upwork and keep business
+          moving
+        </p>
+      </Section>
+      <Layout>
+        <LeftSide></LeftSide>
+        <Main></Main>
+        <RightSide></RightSide>
+      </Layout>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-    padding-top: 52px;
-    max-width: 100%;
-`
+  padding-top: 52px;
+  max-width: 100%;
+`;
 
 const Content = styled.div`
-    max-width: 1128px;
-    margin-left: auto;
-    margin-right: auto;
-`
+  max-width: 1128px;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 const Section = styled.div`
-    min-height: 50px;
-    padding: 16px 0;
-    box-sizing: content-border-box;
-    text-align: center;
-    text-decoration: underline;
+  min-height: 50px;
+  padding: 16px 0;
+  box-sizing: content-border-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  justify-content: center;
+
+  h5 {
+    color: #0a66c2;
+    font-size: 14px;
+    a {
+      font-weight: 700;
+    }
+  }
+
+  P {
+    font-size: 14px;
+    color: #434649;
+    font-weight: 600;
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  margin: 25px 0;
+  @media (max-width: 768px) {
     display: flex;
-    justify-content: center;
-
-    h5{
-        color:#0a66c2;
-        font-size: 14px;
-        a{
-            font-weight: 700;
-        }
-    }
-
-    P {
-        font-size: 14px;
-        color: #434649;
-        font-weight: 600;
-    }
-
-    @media(max-width: 768px){
-        flex-direction: column;
-        padding: 0 5px;
-    }
-`
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
 
 export default Home;
---
+++ src/components/Main.js
@@ -0,0 +1,112 @@
+import styled from "styled-components";
+
+const Main = (props) => {
+  return (
+    <Container>
+      <ShareBox>
+        Share
+        <div>
+          <img src="/images/user.svg" />
+          <button>Start a post</button>
+        </div>
+        <div>
+          <button>
+            <img src="/images/photo-icon.svg" />
+            <span>Photo</span>
+          </button>
+          <button>
+            <img src="/images/video-icon.svg" />
+            <span>Video</span>
+          </button>
+          <button>
+            <img src="/images/event-icon.svg" />
+            <span>Event</span>
+          </button>
+          <button>
+            <img src="/images/article-icon.svg" />
+            <span>Article</span>
+          </button>
+        </div>
+      </ShareBox>
+      <div>
+        <Article>Article</Article>
+      </div>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  grid-area: main;
+`;
+
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+`;
+
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  margin: 0 0 8px;
+  background: white;
+
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 14px;
+      line-height: 1.5;
+      min-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 0px 16px;
+      img {
+        width: 48px;
+        border-radius: 50%;
+        margin-right: 8px;
+      }
+      button {
+        margin: 4px 0;
+        flex-grow: 1;
+        border-radius: 35px;
+        padding-left: 16px;
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        background-color: white;
+        text-align: left;
+      }
+    }
+    &:nth-child(2) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      padding-bottom: 4px;
+
+      button {
+        img {
+          margin: 0 4px 0 -2px;
+        }
+        span {
+          color: #70b5f9;
+        }
+      }
+    }
+  }
+`;
+
+const Article = styled(CommonCard)``;
+
+export default Main;
