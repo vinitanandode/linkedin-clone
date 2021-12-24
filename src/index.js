d09b7edf0359e657087971431ac4cd0edc116c1d
Vinita Nandode <vinita.nandode@gmail.com>
2021-12-24 23:35:59 +0530

    mod5

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="{process.env.PUBLIC_URL}">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
